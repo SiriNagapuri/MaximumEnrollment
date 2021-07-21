6010d800517849881d8ee70ff2644643228159b0
Mahesh Dhikonda <mahesh.dhikonda@hotmail.com>
2021-07-21 22:47:18 +0530

    Add project files.

---
+++ MaximumEnrollment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Departments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Total students placed in CS: ");
+            int cs = int.Parse(Console.ReadLine());
+            Console.Write("Total students placed in MECH: ");
+            int mech = int.Parse(Console.ReadLine());
+            Console.Write("Total students placed in MET: ");
+            int met = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (cs >= mech && cs >= met)
+            {
+                Console.WriteLine( "Highest placement CS");
+            }
+            if (mech >= cs && mech >= met)
+            {
+                Console.WriteLine( "Highest placement MECH");
+            }
+            if (met >= cs && met >= mech)
+            {
+                Console.WriteLine( "Highest placement MET");
+            }
+
+
+        }
+    }
+}
